21f0b8f227bb1c26710a655d75db0812ad720da1
Fatima <alfatima20233@gmail.com>
2025-02-24 13:42:25 +0400

    fatima-front-end

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,10 +1,32 @@
-
+import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import logo from "../Images/logo-t.png";
+import { Link } from "react-router-dom";
 const Header = () => {
-
   return (
-    <h1>Header</h1>
+    <>
+      <Navbar className="header">
+        <Nav>
+          <NavItem>
+            <NavLink>
+              <img src={logo} className="logo-t " />
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active href="#">
+              Profile
+            </NavLink>
+          </NavItem>
 
+          <NavItem>
+            <Link to="/">Home</Link>
+          </NavItem>
 
+          <NavItem>
+            <NavLink href="#">Logout</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </>
   );
 };
 
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,9 +1,49 @@
 import log from "../Images/loginImage.jpg";
+import { Container, Row, Col, Form } from "reactstrap";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
-      <img src={log} />
+      <Container>
+        <img src={log} className="loginImage" />
+
+        <Form>
+          <Row>
+            <Col md={3}>
+              Email
+              <input
+                id="exampleEmail"
+                name="email"
+                placeholder="Enter Email"
+                type="email"
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              Password
+              <input
+                id="examplePassword"
+                name="password"
+                placeholder="Enter password "
+                type="password"
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              <button>Login</button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+
+      <p className="smalltext">
+        No Account? <Link to="/register">Sign Up now.</Link>
+      </p>
     </div>
   );
 };
