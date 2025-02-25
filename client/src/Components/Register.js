03d9834a037282de12ec379edde0e218df53ede3
alfatima05 <alfatima20233@gmail.com>
2025-02-25 23:09:19 +0400

    Add files via upload

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,65 +1,52 @@
-import { Container, Row, Col, Form } from "reactstrap";
+import loginImage from "../Images/loginImage.jpg";
+import {
+  Button,
+  Col,
+  Label,
+  Container,
+  Row,
+  FormGroup,
+  Input,
+  Form,
+} from "reactstrap";
+import logo from "../Images/logo-t.png";
 
 const Register = () => {
   return (
-    <>
+    <Container>
       <h1>Register</h1>
-      <Container>
-        <Form>
-          <Row>
-            <Col>
-              <p>Name</p>
-              <input
-                id="exampleEmail"
-                name="email"
-                placeholder="Enter Name"
-                type="name"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={3}>
-              <p>Email</p>
-              <input
-                id="exampleEmail"
-                name="email"
-                placeholder="Enter Email"
-                type="email"
-              />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={3}>
-              <p>Password</p>
-              <input
-                id="examplePassword"
-                name="password"
-                placeholder="Enter password "
-                type="password"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={3}>
-              <p>Confierm password</p>
-              <input
-                id="examplePassword"
-                name="password"
-                placeholder="Confierm password "
-                type="password"
-              />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={3}>
-              <button>Register</button>
-            </Col>
-          </Row>
-        </Form>
-      </Container>
-    </>
+      <Form>
+        <Row>
+          <Col md={6}>
+            Name<br></br>
+            <Input type="text" name="name"></Input>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            Email<br></br>
+            <Input type="email" name="email"></Input>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            Password<br></br>
+            <Input type="password" name="password"></Input>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            Confirm Password<br></br>
+            <Input type="password" name="confirmpassword"></Input>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Button>Register</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 };
 
