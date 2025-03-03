9a7986fe65b59c172d54334a7494efc27f460942
Fatima <alfatima20233@gmail.com>
2025-03-03 11:05:21 +0400

    validation

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,4 +1,9 @@
 import loginImage from "../Images/loginImage.jpg";
+import { userSchemaValidation } from "../Validations/UserValidations";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import {
   Button,
   Col,
@@ -10,50 +15,38 @@ import {
   Form,
 } from "reactstrap";
 import logo from "../Images/logo-t.png";
-<<<<<<< HEAD
-import { userSchema } from "../Validations/Validations";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-=======
->>>>>>> 03d9834a037282de12ec379edde0e218df53ede3
 
 const Register = () => {
-  //For form validation using react-hook-form
   const {
     register,
-    handleSubmit, // Submit the form when this is called
+    handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(userSchema) });
+  } = useForm({ resolver: yupResolver(userSchemaValidation) });
 
-  //Handel form
+  // Handle form submission
 
   const onSubmit = (data) => {
-    //data from the form
     console.log("Form Data", data); // You can handle the form submission here
   };
 
   return (
     <Container>
       <h1>Register</h1>
-<<<<<<< HEAD
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Row>
           <Col md={6}>
             Name<br></br>
             <input type="text" name="name" {...register("name")}></input>
-            <p className="error">{errors.name?.message}</p>
           </Col>
+          <p className="error">{errors.name?.message}</p>
         </Row>
-
         <Row>
           <Col md={6}>
             Email<br></br>
             <input type="email" name="email" {...register("email")}></input>
-            <p className="error">{errors.email?.message}</p>
           </Col>
+          <p className="error">{errors.email?.message}</p>
         </Row>
-
         <Row>
           <Col md={6}>
             Password<br></br>
@@ -62,10 +55,9 @@ const Register = () => {
               name="password"
               {...register("password")}
             ></input>
-            <p className="error">{errors.password?.message}</p>
           </Col>
+          <p className="error">{errors.password?.message}</p>
         </Row>
-
         <Row>
           <Col md={6}>
             Confirm Password<br></br>
@@ -74,37 +66,9 @@ const Register = () => {
               name="confirmpassword"
               {...register("confirmPassword")}
             ></input>
-            <p className="error">{errors.confirmPassword?.message}</p>
           </Col>
+          <p className="error">{errors.confirmPassword?.message}</p>
         </Row>
-
-=======
-      <Form>
-        <Row>
-          <Col md={6}>
-            Name<br></br>
-            <Input type="text" name="name"></Input>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            Email<br></br>
-            <Input type="email" name="email"></Input>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            Password<br></br>
-            <Input type="password" name="password"></Input>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            Confirm Password<br></br>
-            <Input type="password" name="confirmpassword"></Input>
-          </Col>
-        </Row>
->>>>>>> 03d9834a037282de12ec379edde0e218df53ede3
         <Row>
           <Col md={6}>
             <Button>Register</Button>
