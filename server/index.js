3bbba9779b73b997c5bef98abbf5541c92278527
Fatima <alfatima20233@gmail.com>
2025-04-08 09:48:18 +0400

    add registerUser server API

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import cors from "cors";
 import express from "express";
-import UserModel from "./Models/UserModel.js";
+import UserModel from "./Models/UserModel.js"; //write js
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -11,9 +12,26 @@ app.use(cors());
 const connectString =
   "mongodb+srv://alfatima20233:fatima20233@cluster0.wllrxot.mongodb.net/postITDb?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(connectString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(connectString);
+
+//API routes
+app.post("/registerUser", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const user = new UserModel({
+      name: name, //can write as (name, )
+      email: email,
+      password: hashedpassword,
+    });
+    await user.save();
+    res.send({ user: user, msg: "Added." });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(3001, () => {
