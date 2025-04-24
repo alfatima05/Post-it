9dcc3a71aef26fac5b5fbc46a80617e999f4cf32
Fatima <alfatima20233@gmail.com>
2025-04-24 09:20:27 +0400

    complete act7

--- server/index.js
+++ server/index.js
@@ -85,6 +85,19 @@ app.post("/savePost", async (req, res) => {
   }
 });
 
+//API for display the posts
+app.get("/getPosts", async (req, res) => {
+  try {
+    // Fetch all posts from the "PostModel" collection, sorted by createdAt in descending order
+    const posts = await PostModel.find({}).sort({ createdAt: -1 });
+    const countPost = await PostModel.countDocuments({});
+    res.send({ posts: posts, count: countPost });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 app.listen(3001, () => {
   console.log("You are connected , Thnak you ");
 });
